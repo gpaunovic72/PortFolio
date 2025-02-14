b7e0cf541a63ddd81c6f9772c42c074226f79abc
Goran Paunovic <g.paunovic.8@gmail.com>
2025-02-14 10:38:23 +0100

    Ajout des difficultés rencontré au niveau des différents projets

--- src/data/projectCard.js
+++ src/data/projectCard.js
@@ -9,7 +9,7 @@ export const cards = [
     ],
     picture: "../../../public/assets/Booki.webp",
     description:
-      "Pour ce projet, j'ai été chargé d'intégrer l'interface du site Booki en HTML et CSS, en respectant la maquette fournie ainsi que les principes du responsive design.\n\n J'ai suivi les spécifications techniques détaillées et assuré le versionnement du projet sur GitHub. ",
+      "Pour ce projet, j'ai été chargé d'intégrer l'interface du site Booki en HTML et CSS, en respectant la maquette fournie ainsi que les principes du responsive design.\n\n J'ai suivi les spécifications techniques détaillées et assuré le versionnement du projet sur GitHub.\n\n J'ai rencontré des difficultés pour adapter les cartes et éviter que les éléments ne se chevauchent ou ne débordent. Pour résoudre ce problème, j'ai utilisé les flexbox et défini les largeurs en pourcentages, ce qui m'a permis de créer une mise en page fluide et réactive, garantissant que les éléments s'ajustent correctement sur différentes tailles d'écran. ",
     lien: "https://github.com/gpaunovic72/Booki",
   },
   {
@@ -27,7 +27,7 @@ export const cards = [
       "../../../public/assets/sophie-bluel-3.webp",
     ],
     description:
-      "Pour ce projet, j'ai rejoint l'équipe front-end de l'agence ArchiWebos pour contribuer au développement du site portfolio d'une architecte d'intérieur.\n\n Mes missions consistaient à intégrer la page de présentation des travaux, concevoir la page de connexion de l’administrateur, et développer une modale d’upload de médias.\n\n J’ai travaillé à partir des maquettes Figma, en utilisant une API fournie ainsi que le code back-end pour la gestion des données, tout en suivant l’avancement des tâches via le Kanban.",
+      "Pour ce projet, j'ai rejoint l'équipe front-end de l'agence ArchiWebos pour contribuer au développement du site portfolio d'une architecte d'intérieur.\n\n Mes missions consistaient à intégrer la page de présentation des travaux, concevoir la page de connexion de l’administrateur, et développer une modale d’upload de médias.\n\n J’ai travaillé à partir des maquettes Figma, en utilisant une API fournie ainsi que le code back-end pour la gestion des données, tout en suivant l’avancement des tâches via le Kanban.\n\n J'ai rencontré des difficultés pour récupérer les données du formulaire. Pour résoudre ce problème, j'ai utilisé la méthode FormData, ce qui m'a permis de collecter efficacement les informations soumises. ",
     lien: "https://github.com/gpaunovic72/Portfolio-architecte-sophie-bluel",
   },
   {
@@ -41,7 +41,7 @@ export const cards = [
     ],
     picture: "../../../public/assets/Nina Carducci.webp",
     description:
-      "Pour ce projet, j’ai été missionné en tant que développeur web freelance pour optimiser le site de Nina Carducci, une photographe professionnelle.\n\n Mon travail consistait à améliorer les performances et le référencement SEO du site en optimisant les images, le code et en mettant en place un référencement local avec Schema.org.\n\n J’ai également ajouté les métadonnées pour les réseaux sociaux et effectué des modifications liées à l’accessibilité. En parallèle, j’ai corrigé plusieurs bugs d’affichage et de navigation dans la galerie d’images.\n\n Toutes les améliorations ont été documentées dans un rapport détaillé comparant les performances avant et après optimisation.",
+      "Pour ce projet, j’ai été missionné en tant que développeur web freelance pour optimiser le site de Nina Carducci, une photographe professionnelle.\n\n Mon travail consistait à améliorer les performances et le référencement SEO du site en optimisant les images, le code et en mettant en place un référencement local avec Schema.org.\n\n J’ai également ajouté les métadonnées pour les réseaux sociaux et effectué des modifications liées à l’accessibilité. En parallèle, j’ai corrigé plusieurs bugs d’affichage et de navigation dans la galerie d’images.\n\n Toutes les améliorations ont été documentées dans un rapport détaillé comparant les performances avant et après optimisation.\n\n J'ai rencontré des difficultés pour mettre en place le référencement local. Pour y remédier, j'ai utilisé des balises schema.org pour structurer les informations locales, ainsi que des balises Open Graph et Twitter Cards pour optimiser le partage sur les réseaux sociaux. ",
     lien: "https://github.com/gpaunovic72/Nina_Carducci",
   },
   {
@@ -60,7 +60,7 @@ export const cards = [
       "../../../public/assets/Kasa-4.webp",
     ],
     description:
-      "Pour ce projet, J’ai été missionné en tant que développeur front-end freelance pour participer à la refonte du site Kasa en React.\n\n Mes missions étaient de Créer l’application React, Développer les composants en suivant la maquette Figma, Mettre en place le routing avec React Router, Implémenter la galerie d’images et Assurer un design responsive et optimisé.\n\n Le projet a été développé avec des données JSON temporaires, en attendant le back-end en Node.js.",
+      "Pour ce projet, J’ai été missionné en tant que développeur front-end freelance pour participer à la refonte du site Kasa en React.\n\n Mes missions étaient de Créer l’application React, Développer les composants en suivant la maquette Figma, Mettre en place le routing avec React Router, Implémenter la galerie d’images et Assurer un design responsive et optimisé.\n\n Le projet a été développé avec des données JSON temporaires, en attendant le back-end en Node.js.\n\n J'ai rencontré des difficultés pour éviter la répétition de code. Pour y remédier, j'ai mis en place des composants réutilisables ainsi que des mixins et des variables pour le CSS.",
     lien: "https://github.com/gpaunovic72/Kasa",
   },
   {
@@ -81,7 +81,7 @@ export const cards = [
       "../../../public/assets/mon-vieux-grimoire-5.webp",
     ],
     description:
-      "Pour ce projet,J’ai été recruté en tant que développeur back-end freelance pour collaborer avec Kévin, un développeur front-end expérimenté, sur le projet Mon Vieux Grimoire, un site de notation et de référencement de livres pour une chaîne de librairies à Lille.\n\n Mes missions étaient de Développer l’API permettant aux utilisateurs d’ajouter et noter des livres, Assurer l’intégration avec le front-end, déjà développé en React, Respecter les spécifications techniques fournies et Optimiser les images envoyées par les utilisateurs pour respecter les bonnes pratiques du Green Code.   ",
+      "Pour ce projet,J’ai été recruté en tant que développeur back-end freelance pour collaborer avec Kévin, un développeur front-end expérimenté, sur le projet Mon Vieux Grimoire, un site de notation et de référencement de livres pour une chaîne de librairies à Lille.\n\n Mes missions étaient de Développer l’API permettant aux utilisateurs d’ajouter et noter des livres, Assurer l’intégration avec le front-end, déjà développé en React, Respecter les spécifications techniques fournies et Optimiser les images envoyées par les utilisateurs pour respecter les bonnes pratiques du Green Code.\n\n J'ai rencontré des difficultés pour l'upload et l'optimisation des images. Pour y remédier, j'ai mis en place un middleware Multer pour gérer l'upload des images et utilisé Sharp pour les optimiser.",
     lien: "https://github.com/gpaunovic72/Mon_Vieux_Grimoire",
   },
   {
@@ -99,7 +99,7 @@ export const cards = [
       "../../../public/assets/Portfolio-2.webp",
     ],
     description:
-      "Pour ce projet, ma mission était de créer mon portfolio, en veillant à ce qu’il soit responsive et accessible en ligne.",
+      "Pour ce projet, ma mission était de créer mon portfolio, en veillant à ce qu’il soit responsive et accessible en ligne.\n\n J'ai rencontré des difficultés avec la fonction useExperiences pour gérer efficacement les années et les mois d'expérience. Pour résoudre ce problème, j'ai utilisé useMemo pour mémoriser les années extraites des expériences, ce qui a amélioré les performances en évitant des calculs répétitifs. De plus, j'ai optimisé la fonction isMonthColored pour déterminer la couleur des mois en fonction des dates de début et de fin des expériences, garantissant ainsi une meilleure réactivité de l'application.",
     lien: "https://github.com/gpaunovic72/PortFolio",
   },
 ];
